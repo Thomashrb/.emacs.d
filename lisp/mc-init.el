e6a68195770a42b4a79c8bdd593dc509efd21006
Thomashrb <thomas_laammas@hotmail.com>
2017-11-13 16:27:39 +0100

    added multiple cursors

---
+++ lisp/mc-init.el
@@ -0,0 +1,10 @@
+;; Multiple cursors
+(use-package multiple-cursors
+  :ensure t
+  :config
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->")         'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
+  (global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
+  (global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this))
