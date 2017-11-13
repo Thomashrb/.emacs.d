b3572a85d1dd9621a3129ae52c742e546982b352
Thomashrb <thomas_laammas@hotmail.com>
2017-11-13 16:09:18 +0100

    added magit

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 (load "~/.emacs.d/lisp/ivy-init.el")
 (load "~/.emacs.d/lisp/ivymodes/swiper-init.el")
 (load "~/.emacs.d/lisp/ivymodes/councel-init.el")
+(load "~/.emacs.d/lisp/magit-init.el")
 
 ;; Load packages that arent configured
 
@@ -22,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (counsel nyan-mode swiper ivy doom-themes))))
+ '(package-selected-packages (quote (magit counsel nyan-mode swiper ivy doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/magit-init.el
@@ -0,0 +1,10 @@
+;; magit requres cl-lib but it wont install automaticaly on windows
+;;(use-package cl-lib
+;;  :ensure t)
+
+(use-package magit
+  :ensure t
+  :config
+  (setq magit-auto-revert-mode nil)
+  (global-set-key (kbd "C-c m s") 'magit-status)
+  (global-set-key (kbd "C-c m l") 'magit-log))
