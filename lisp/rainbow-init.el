e742f51d31341863da5ad6585592521c7397fe3f
Thomashrb <thomas_laammas@hotmail.com>
2017-11-14 18:54:38 +0100

    added hook so rainbow delimiters will load on all buffer loads

--- lisp/rainbow-init.el
+++ lisp/rainbow-init.el
@@ -1,6 +1,6 @@
 ;; Rainbowdelimiters - Visualy pair up delimiters
 (use-package rainbow-delimiters
   :ensure t)
-(rainbow-delimiters-mode 1)
+(add-hook 'buffer-list-update-hook 'rainbow-delimiters-mode)
 ;; enable in programming mode only
 ;(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
