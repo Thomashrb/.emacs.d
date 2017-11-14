442e4ece26fd6c5378ff260c70b2748e503f804c
Thomashrb <thomas_laammas@hotmail.com>
2017-11-14 16:42:27 +0100

    rainbow-delimiters-mode was not loading, fixed

--- lisp/rainbow-init.el
+++ lisp/rainbow-init.el
@@ -1,7 +1,6 @@
 ;; Rainbowdelimiters - Visualy pair up delimiters
 (use-package rainbow-delimiters
-  :ensure t
-  :config
-  (rainbow-delimiters-mode 1))
+  :ensure t)
+(rainbow-delimiters-mode 1)
 ;; enable in programming mode only
 ;(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
