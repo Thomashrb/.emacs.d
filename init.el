c203f25eb64632e24f92bb203c9c6c39a795c44a
Thomashrb <thomas_laammas@hotmail.com>
2017-11-14 16:42:54 +0100

    moved statusbar and theme into its own files

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
 (load "~/.emacs.d/lisp/myinit.el")
 (load "~/.emacs.d/lisp/general.el")
 (load "~/.emacs.d/lisp/appearance.el")
+(load "~/.emacs.d/lisp/theme.el")
+(load "~/.emacs.d/lisp/modeline.el")
 (load "~/.emacs.d/lisp/ivy-init.el")
 (load "~/.emacs.d/lisp/ivymodes/swiper-init.el")
 (load "~/.emacs.d/lisp/ivymodes/councel-init.el")
@@ -27,6 +29,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
     (smart-mode-line markdown-mode rainbow-delimiters rainbow-delimiters-mode haskell-mode ensime multiple-cursors magit counsel nyan-mode swiper ivy doom-themes))))
--- lisp/appearance.el
+++ lisp/appearance.el
@@ -7,31 +7,3 @@
 (scroll-bar-mode -1)
 (set-frame-font "Inconsolata 16" nil t)
 
-;; Statusbar
-(use-package nyan-mode
-  :ensure t
-  :config
-  (nyan-mode 1))
-
-;; Theme
-(require 'doom-themes)
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
-;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-;; may have their own settings.
-(load-theme 'doom-one t)
-;; Enable flashing mode-line on errors
-(doom-themes-visual-bell-config)
-;; Enable custom neotree theme
-(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
-;; Corrects (and improves) org-mode's native fontification.
-(doom-themes-org-config)
-(custom-set-variables
-  '(package-selected-packages (quote (doom-themes))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
