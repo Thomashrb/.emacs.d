a7d8a9b4e48ea7ffe952c4594a5a1d92db749ee5
Thomashrb <thomas_laammas@hotmail.com>
2017-11-14 18:58:56 +0100

    added explanitory comment

--- lisp/languages/haskell-init.el
+++ lisp/languages/haskell-init.el
@@ -1,13 +1,11 @@
 ;; Haskell mode - IDE like experience for Haskell
 (use-package haskell-mode
   :ensure t)
-
+;; haskell-mode has its own hooks
 (add-hook 'haskell-mode-hook 'haskell-indent-mode)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'haskell-doc-mode)
-
 ;; Ghc-mod
 ;(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 ;(autoload 'ghc-init "ghc" nil t);
 ;(autoload 'ghc-debug "ghc" nil t)
-
