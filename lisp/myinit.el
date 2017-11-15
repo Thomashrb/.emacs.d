c416abc7b61b48176c471038bc9f91135e196a16
Thomashrb <thomas_laammas@hotmail.com>
2017-11-15 17:40:12 +0100

    add stable melpa

--- lisp/myinit.el
+++ lisp/myinit.el
@@ -6,8 +6,11 @@
   (add-to-list 'package-archives (cons "melpa" url) t))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 ;; Get use-package so we can pull the rest of ouer deps
 (unless (package-installed-p 'use-package)
