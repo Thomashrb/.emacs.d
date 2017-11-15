d7ef0af519eac1558978906dbcedd7b2f8eae485
Thomashrb <thomas_laammas@hotmail.com>
2017-11-16 00:47:10 +0100

    cleaned init.el switched theme to moe

--- init.el
+++ init.el
@@ -25,24 +25,3 @@
 (load "~/.emacs.d/lisp/languages/scala-init.el")
 (load "~/.emacs.d/lisp/languages/haskell-init.el")
 (load "~/.emacs.d/lisp/languages/markdown-init.el")
-
-
-;; Junk from emacs
-;; Do not remove this
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
- '(package-selected-packages
-   (quote
-    (nav dirtree counsel-projectile neotree smart-mode-line markdown-mode rainbow-delimiters rainbow-delimiters-mode haskell-mode ensime multiple-cursors magit counsel nyan-mode swiper ivy doom-themes))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
