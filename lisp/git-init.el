e18169eb71cb1e035657b70e4a885f8e76468a42
Thomashrb <thomas_laammas@hotmail.com>
2017-11-18 12:19:41 +0100

    rename

--- lisp/git-init.el
+++ lisp/git-init.el
@@ -1,4 +1,5 @@
-;; magit requres cl-lib but it wont install automaticaly on windows
+;; Magit
+;  Magit requres cl-lib but it wont install automaticaly on windows
 (use-package cl-lib
   :ensure t)
 
@@ -8,3 +9,8 @@
   (setq magit-auto-revert-mode nil)
   (global-set-key (kbd "C-c m s") 'magit-status)
   (global-set-key (kbd "C-c m l") 'magit-log))
+
+;; Gitgutter
+(use-package git-gutter
+  :ensure t)
+(global-git-gutter-mode t)
