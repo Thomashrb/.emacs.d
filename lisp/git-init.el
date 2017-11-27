acec4259d460846a663c646abacb05bfd41ce617
Thomashrb <thomas_laammas@hotmail.com>
2017-11-27 11:20:06 +0100

    clean

--- lisp/git-init.el
+++ lisp/git-init.el
@@ -3,15 +3,15 @@
 (use-package cl-lib
   :ensure t)
 
-;; Git porcelain
-(use-package magit
-  :ensure t
-  :config
-  (setq magit-auto-revert-mode nil)
-  (global-set-key (kbd "C-c m s") 'magit-status)
-  (global-set-key (kbd "C-c m l") 'magit-log))
-
 ;; Sidebar with gitmarkers
 (use-package git-gutter
   :ensure t)
-(global-git-gutter-mode t)
+
+;; Git porcelain
+(use-package magit
+  :ensure t
+  :init
+  (setq magit-auto-revert-mode nil)
+  (global-set-key (kbd "C-c m s") 'magit-status)
+  (global-set-key (kbd "C-c m l") 'magit-log)
+  (global-git-gutter-mode t))
--- lisp/nav.el
+++ lisp/nav.el
@@ -1,4 +1,5 @@
 (use-package nav
-  :ensure t)
+  :ensure t
 ;;(nav-disable-overeager-window-splitting)
-(global-set-key (kbd "<f8>") 'nav-toggle)
+  :config
+  (global-set-key (kbd "<f8>") 'nav-toggle))
