9b5916bc45e651dcb6c005f3cdae63c2c8d79fd9
thomashrb <taammas.hrb@gmail.com>
2017-11-28 00:15:14 +0100

    company needs to load independently

--- lisp/languages/nix-init.el
+++ lisp/languages/nix-init.el
@@ -3,6 +3,6 @@
   :ensure t)
 
 (use-package company-nixos-options
-  :ensure t
-  :config
-  (add-to-list 'company-backends 'company-nixos-options))
+  :ensure t)
+
+(add-to-list 'company-backends 'company-nixos-options)
