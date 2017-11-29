c205dc494f1b1370055e9b37d330f76b8b2924b2
Thomashrb <thomas_laammas@hotmail.com>
2017-11-29 14:15:20 +0100

    improove haskell dev

--- lisp/languages/haskell-init.el
+++ lisp/languages/haskell-init.el
@@ -1,3 +1,16 @@
+;; Prerecs for haskell mode setup
+(use-package hindent
+  :ensure t)
+
+(use-package company-ghc
+  :ensure t)
+
+(use-package shm
+  :ensure t)
+
+(use-package ghc
+  :ensure t)
+
 ;; Haskell mode - IDE like experience for Haskell
 (use-package haskell-mode
   :ensure t
@@ -6,7 +19,8 @@
   (custom-set-variables '(company-ghc-show-info t)))
 
 ;; haskell-mode has its own hooks
-(add-hook 'haskell-mode-hook 'haskell-indent-mode)
+;;(add-hook 'haskell-mode-hook 'haskell-indent-mode)
+(add-hook 'haskell-mode-hook 'structured-haskell-mode) ;; use this SHM instead
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'haskell-doc-mode)
 ;; Ghc-mod
