9489a6724f8c523198aa2e3f4f9e05c5dfdc3b71
Thomashrb <thomas_laammas@hotmail.com>
2017-11-30 10:57:17 +0100

    streamline haskell mode

--- lisp/languages/haskell-init.el
+++ lisp/languages/haskell-init.el
@@ -5,9 +5,6 @@
 (use-package company-ghc
   :ensure t)
 
-(use-package shm
-  :ensure t)
-
 (use-package ghc
   :ensure t)
 
@@ -23,11 +20,11 @@
         (lambda (args) (apply 'nix-shell-command (nix-current-sandbox) args)))
 
 ;; haskell-mode has its own hooks
-;;(add-hook 'haskell-mode-hook 'haskell-indent-mode)
-(add-hook 'haskell-mode-hook 'structured-haskell-mode) ;; use this SHM instead
+(add-hook 'haskell-mode-hook 'haskell-indent-mode)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'haskell-doc-mode)
+
 ;; Ghc-mod
-;(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-;(autoload 'ghc-init "ghc" nil t);
-;(autoload 'ghc-debug "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(autoload 'ghc-init "ghc" nil t);
+(autoload 'ghc-debug "ghc" nil t)
