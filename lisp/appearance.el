87203c1ad3e8d1a253c49d69eb163c5a50125588
Thomashrb <thomas_laammas@hotmail.com>
2017-11-30 10:56:56 +0100

    add font for --daemon

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -6,4 +6,4 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (set-frame-font "Inconsolata 13" nil t)
-
+(setq default-frame-alist '((font . "Inconsolata 13"))) ;;; set default font for emacs --daemon / emacsclient
