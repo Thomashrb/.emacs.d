2ab231cd119998a6affb0adb40cfbb607860ceb9
Thomashrb <thomas_laammas@hotmail.com>
2017-12-04 16:37:10 +0100

    cleaned

--- lisp/myinit.el
+++ lisp/myinit.el
@@ -3,14 +3,15 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
+  (add-to-list 'package-archives (cons "melpa" url) t) ;; unstable
+  (add-to-list 'package-archives
+               '("melpa-stable" . "https://stable.melpa.org/packages/") t) ;; stable
+)
+
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
-
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 ;; Get use-package so we can pull the rest of ouer deps
 (unless (package-installed-p 'use-package)
