c9c257ac8cda7188db84962d4063ebc1442f29f9
Thomashrb <thomas_laammas@hotmail.com>
2017-12-11 23:50:20 +0100

    fixed startup hiccup

--- lisp/languages/python-init.el
+++ lisp/languages/python-init.el
@@ -3,9 +3,8 @@
   :ensure t
   :defer t
   :init
-  (add-to-list 'company-backends 'company-anaconda)
+  ;(add-to-list 'company-backends 'company-anaconda)
   (add-hook 'python-mode-hook 'anaconda-mode))
 
 (use-package company-anaconda
-  :ensure t
-  :defer t)
+  :ensure t)
--- lisp/myinit.el
+++ lisp/myinit.el
@@ -1,8 +1,15 @@
 ;; Melpa repo
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages") t) ;; unstable
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t) ;; stable
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
 
 ;; Get use-package so we can pull the rest of ouer deps
 (unless (package-installed-p 'use-package)
