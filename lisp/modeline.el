a68a5eb667908c25cc0f0df9fa47abb7cb73cb55
Thomashrb <thomas_laammas@hotmail.com>
2017-12-12 00:05:31 +0100

    fixed formating

--- lisp/modeline.el
+++ lisp/modeline.el
@@ -4,11 +4,13 @@
 ;; (nyan-mode 1)
 
 ;; Spaceline
-(use-package spaceline :ensure t
+(use-package spaceline
+  :ensure t
   :config
   (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
 
 ;; Needed for spaceline to work
-(use-package spaceline-config :ensure spaceline
+(use-package spaceline-config
+  :ensure spaceline
   :config
   (spaceline-emacs-theme))
