4a4b5e60bafc3fb2a7bf75ccb8e838da42283335
Thomashrb <thomas_laammas@hotmail.com>
2017-12-20 22:50:48 +0100

    more descriptive name

---
+++ lisp/cleanup.el
@@ -0,0 +1,4 @@
+;; Make backupdir and dont litter withbackupfiles
+(defvar backup-dir "~/.emacs.d/backups/")
+(setq backup-directory-alist (list (cons "." backup-dir)))
+(setq make-backup-files nil)
