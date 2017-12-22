a8758a6ac11f496b29b97068084b84c8fe49dc02
Thomashrb <thomas@beat.no>
2017-12-22 14:22:03 +0100

    removed "#" that I dont know what does and  added helm mini shortcut

--- lisp/helm.el
+++ lisp/helm.el
@@ -6,9 +6,10 @@
    (setq helm-buffers-fuzzy-matching t)
    (setq helm-autoresize-mode t)
    (setq helm-buffer-max-length 40)
-   (global-set-key (kbd "M-x") #'helm-M-x)
-   (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-   (global-set-key (kbd "C-x C-f") #'helm-find-files))
+   (global-set-key (kbd "M-x") 'helm-M-x)
+   (global-set-key (kbd "C-x b") 'helm-mini)
+   (global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+   (global-set-key (kbd "C-x C-f") 'helm-find-files))
 
 (use-package helm-swoop
    :ensure t
