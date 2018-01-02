623f5d4f8a6dd85bcc01f1626c8c37c3bad9152e
Thomashrb <thomas_laammas@hotmail.com>
2018-01-02 20:53:57 +0100

    startuptime fixed, cleaned

--- lisp/languages/nix-init.el
+++ lisp/languages/nix-init.el
@@ -1,12 +1,17 @@
-;; TODO - can I speed launch up by altering this?
-
 ;; Collection of usefullness when interacting with nix
-(use-package nix-mode ;;
+(use-package nix-mode
   :ensure t
-  :defer t)
+  :defer t
+  :init
+  (global-set-key (kbd "C-c C-S-n") 'helm-nixos-options)
+;  (add-to-list 'company-backends 'company-nixos-options)
+  )
+
+(use-package nixos-options
+  :ensure t)
+
+(use-package helm-nixos-options
+  :ensure t)
 
 (use-package company-nixos-options
-  :ensure t
-  :defer t)
-
-;(add-to-list 'company-backends 'company-nixos-options)
+  :ensure t)
