059d87fa4c1f057eb08eff21052e670fcd66f69e
Thomashrb <thomas_laammas@hotmail.com>
2018-01-02 20:55:15 +0100

    clean

--- lisp/languages/rust-init.el
+++ lisp/languages/rust-init.el
@@ -1,19 +1,16 @@
 ;; from: http://julienblanchard.com/2016/fancy-rust-development-with-emacs/
 (use-package rust-mode
   :ensure t
-  :defer t)
+  :defer t
+  :init
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup) ;; Flycheck
+  (add-hook 'rust-mode-hook 'cargo-minor-mode)
+  (add-hook 'rust-mode-hook
+	    (lambda ()
+	      (local-set-key (kbd "C-c <tab>") #'rust-format-buffer))))
 
-;(use-package cargo
-;  :ensure t
-;  :defer t)
-
-;; start flycheck-rust
-;(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-
-;; install package rustfmt for formating
-;; (add-hook 'rust-mode-hook
-;;           (lambda ()
-;;             (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+(use-package cargo
+  :ensure t)
 
 ;; install package racer for code completion
 ;; (setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
