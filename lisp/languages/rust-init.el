68a11a46fb72c80ce8cdc422e69b91d76f33750a
Thomashrb <thomas_laammas@hotmail.com>
2018-01-03 13:23:17 +0100

    Add TODO, now works without racer

--- lisp/languages/rust-init.el
+++ lisp/languages/rust-init.el
@@ -7,16 +7,22 @@
   (add-hook 'rust-mode-hook 'cargo-minor-mode)
   (add-hook 'rust-mode-hook
 	    (lambda ()
-	      (local-set-key (kbd "C-c <tab>") #'rust-format-buffer))))
+	      (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+;;  (add-hook 'rust-mode-hook #'racer-mode)
+;;  (add-hook 'racer-mode-hook #'eldoc-mode)
+;;  (add-hook 'racer-mode-hook #'company-mode)
+;;  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+  )
 
 (use-package cargo
   :ensure t)
 
-;; install package racer for code completion
-;; (setq racer-cmd "~/.cargo/bin/racer") ;; Rustup binaries PATH
-;; (setq racer-rust-src-path "/home/bbsl/Code/rust/src") ;; Rust source code PATH
-
-;; (add-hook 'rust-mode-hook #'racer-mode)
-;; (add-hook 'racer-mode-hook #'eldoc-mode)
-;; (add-hook 'racer-mode-hook #'company-mode)
+;;TODO set up racer
+;; (use-package racer
+;;   :ensure t
+;;   :defer t
+;;   :init
+;;   (setq racer-cmd "/nix/store/xgzhmzialg4aj2c70lmlk7dcf29p99b0-rustup-1.3.0") ;; Rustup binaries PATH
+;;   ;;(setq racer-rust-src-path "/home/bbsl/Code/rust/") ;; Rust source code PATH
+;;   )
 
