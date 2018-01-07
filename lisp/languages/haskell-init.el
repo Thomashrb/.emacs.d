aa652b7371db8055a92b9ee0e42a15ee3eaee929
Thomashrb <thomas_laammas@hotmail.com>
2018-01-07 18:50:22 +0100

    disable company-ghc since it atm does not work in nixos

--- lisp/languages/haskell-init.el
+++ lisp/languages/haskell-init.el
@@ -1,21 +1,24 @@
 ;; Dante should pull these packages in
 ;; Note: For this to work as intended with nix-shell
 ;; needs a shell.nix file
-;; test that this works by invoking nix-shell with no options      
+;; test that this works by invoking nix-shell with no options
 ;; ---------------------
 (use-package haskell-mode
-  :defer t)
+  :ensure t)
 
 (use-package ghc
   :ensure t)
 ;;----------------------
 
+;(use-package company-ghc
+;  :ensure t)
+
 (use-package dante
-  :defer t
+  :ensure t
   :after haskell-mode
   :commands 'dante-mode
   :config
-  (add-to-list 'company-backends 'company-ghc)
+;;  (add-to-list 'company-backends 'company-ghc)
   (add-hook 'haskell-mode-hook 'dante-mode)
   (add-hook 'haskell-mode-hook 'flycheck-mode)
   ;; to use hlint
