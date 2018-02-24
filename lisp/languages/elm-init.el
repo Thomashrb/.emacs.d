bae9273a8db1d6b1cad9a609e430e296b2b4b565
Thomashrb <thomas@beat.no>
2018-02-24 11:58:13 +0100

    add flyheck hooks

--- lisp/languages/elm-init.el
+++ lisp/languages/elm-init.el
@@ -1,9 +1,15 @@
 ;; http://www.lambdacat.com/post-modern-emacs-setup-for-el/
 (use-package elm-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-hook 'elm-mode-hook 'flycheck-mode)
+  )
 
 (use-package flycheck-elm
-  :ensure t)
+  :ensure t
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-elm-setup)
+  )
 
 ;; npm install -g elm-oracle
 ;(use-package elm-oracle
