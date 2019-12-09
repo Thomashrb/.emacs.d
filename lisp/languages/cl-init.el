c8d0ab89095a8124554f358884d43e9ae35e17b4
Thomas <thomas@beat.no>
2019-12-09 21:51:55 +0100

    cl-init: add common-lisp-yasnippet

--- lisp/languages/cl-init.el
+++ lisp/languages/cl-init.el
@@ -2,3 +2,9 @@
   :config
   (setq inferior-lisp-program "/usr/bin/sbcl")
   (setq slime-contribs '(slime-fancy slime-quicklisp slime-asdf slime-company)))
+
+(use-package common-lisp-snippets
+  :after (yasnippet))
+
+;; After installing: https://www.quicklisp.org/beta/
+;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
