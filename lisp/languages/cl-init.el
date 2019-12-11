9516bb9139ee2a306c2316890c35da7d12dc4713
Thomas <thomas@beat.no>
2019-12-11 14:16:07 +0100

    cl-init: add slime-company and defer

--- lisp/languages/cl-init.el
+++ lisp/languages/cl-init.el
@@ -3,7 +3,12 @@
   (setq inferior-lisp-program "/usr/bin/sbcl")
   (setq slime-contribs '(slime-fancy slime-quicklisp slime-asdf slime-company)))
 
+(use-package slime-company
+  :defer t
+  :after (slime))
+
 (use-package common-lisp-snippets
+  :defer t
   :after (yasnippet))
 
 ;; After installing: https://www.quicklisp.org/beta/
