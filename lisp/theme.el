62f605838d3aab4d4d5c7a9e8e601d39b44765e7
Thomas <thomas@beat.no>
2019-12-13 12:18:31 +0100

    theme: switch back to spacemacs

--- lisp/theme.el
+++ lisp/theme.el
@@ -2,14 +2,9 @@
 ;; Theme                       ;;
 ;;-----------------------------;;
 
-(use-package zenburn-theme
+(use-package spacemacs-theme
   :defer t
   :init
-  (load-theme 'zenburn t))
-
-;; (use-package spacemacs-theme
-;;   :defer t
-;;   :init
-;;   (load-theme 'spacemacs-dark t)
-;;   (setq spacemacs-theme-org-agenda-height nil)
-;;   (setq spacemacs-theme-org-height nil))
+  (load-theme 'spacemacs-dark t)
+  (setq spacemacs-theme-org-agenda-height nil)
+  (setq spacemacs-theme-org-height nil))
