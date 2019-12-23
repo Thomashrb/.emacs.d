bdb42102a8ab15732da5525abddba8da7b9b37fe
Thomas <thomas@beat.no>
2019-12-23 02:01:04 +0100

    appearance: switch to terminus font

--- lisp/appearance.el
+++ lisp/appearance.el
@@ -5,8 +5,8 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(set-frame-font "Inconsolata 11" nil t)
-(setq default-frame-alist '((font . "Inconsolata 11"))) ;;; set default font for emacs --daemon / emacsclient
+(set-frame-font "xos4 Terminus 11" nil t)
+(setq default-frame-alist '((font . "xos4 Terminus 11"))) ;;; set default font for emacs --daemon / emacsclient
 
 ;; Highlight trailing whitespace
 (setq-default show-trailing-whitespace t)
