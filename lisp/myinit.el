1ce1498271884040034ec30d583b6a9e80ff30f7
Thomas <thomas@beat.no>
2020-08-13 13:16:25 +0200

    remove redundant package-initialize call

--- lisp/myinit.el
+++ lisp/myinit.el
@@ -6,8 +6,6 @@
         ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa"        . "https://melpa.org/packages/")))
 
-(package-initialize)
-
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
