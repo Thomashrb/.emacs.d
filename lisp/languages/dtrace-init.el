3029ecbdb9323d0139beda8820e229a26a266fb9
thomashrb <thomashrb@protonmail.com>
2020-09-17 20:42:50 +0200

    dtrace-init.el: turn off indent

--- lisp/languages/dtrace-init.el
+++ lisp/languages/dtrace-init.el
@@ -1,3 +1,7 @@
 (use-package dtrace-script-mode
   :defer t
-  :mode ("\\.d\\'" . dtrace-script-mode))
+  :mode ("\\.d\\'" . dtrace-script-mode)
+  :config
+  ;; turn off indent for cmode which dtrace-mode inherits
+  (add-to-list 'c-mode-common-hook
+               (lambda () (setq c-syntactic-indentation nil))))
