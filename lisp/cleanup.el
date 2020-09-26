dce72d62733d801fde26e8d6c30653225c1fac36
Thomas <thomas@beat.no>
2020-09-26 12:54:13 +0200

    cleanup: make backups but in backups directory

--- lisp/cleanup.el
+++ lisp/cleanup.el
@@ -1,4 +1,14 @@
 ;; Make backupdir and dont litter withbackupfiles
 (defvar backup-dir "~/.emacs.d/backups/")
+;; safer way to backup
+(setq backup-by-copying t)
+;; faster way t backup
+;(setq backup-by-copying-when-linked t)
 (setq backup-directory-alist (list (cons "." backup-dir)))
-(setq make-backup-files nil)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
+;; turn off backups
+;(setq make-backup-files nil)
