967509caf2bef11b084e412ad394f2152d36ce7c
Thomas <thomas@beat.no>
2020-10-01 18:02:45 +0200

    theme: delete broken theme config

--- lisp/theme.el
+++ lisp/theme.el
@@ -14,9 +14,6 @@
   (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
   (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
 
-  ;; Choose a color for mode-line.(Default: blue)
-  (moe-theme-set-color 'cyan)
-
   ;; Choose what you like, (moe-light) or (moe-dark)
   (moe-dark)
 )
