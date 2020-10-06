9e791ed5fa82d9d0b26e54e2db7ee2e7b536d367
Thomas <thomas@beat.no>
2020-10-06 10:20:36 +0200

    theme: set modeline to yellow

--- lisp/theme.el
+++ lisp/theme.el
@@ -14,6 +14,6 @@
   (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
   (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
 
+  (moe-theme-apply-color 'yellow)
   ;; Choose what you like, (moe-light) or (moe-dark)
-  (moe-dark)
-)
+  (moe-dark))
