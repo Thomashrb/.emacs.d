dedacf4489d6738e5acf4acc352bcb9b6b626864
Thomas <thomas@beat.no>
2020-10-19 15:43:40 +0200

    jira: add custom jql

--- lisp/jira.el
+++ lisp/jira.el
@@ -1,4 +1,9 @@
 ;; (make-directory "~/.org-jira")
 (use-package org-jira
   :config
-  (setq jiralib-url "https://beattech.atlassian.net"))
+  (setq jiralib-url "https://beattech.atlassian.net")
+  (setq org-jira-custom-jqls
+        '(
+          (:jql "project = BP AND labels = Content ORDER BY created DESC"
+                :filename "BP_content")
+          )))
