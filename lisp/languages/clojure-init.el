d9c4e6b5e461908298ae1f4a311936e70f3fd901
Thomas <thomas@beat.no>
2020-11-03 09:51:35 +0100

    clojure-init.el: add clj-refactor and refactor keybind

--- lisp/languages/clojure-init.el
+++ lisp/languages/clojure-init.el
@@ -1,6 +1,18 @@
-(use-package clojure-mode
+(use-package clj-refactor
   :defer t)
 
 (use-package cider
   :defer t
-  :after (clojure-mode))
+  :bind
+  (:map cider-mode-map
+        ("C-c C-f" . cider-format-buffer)))
+
+(use-package clojure-mode
+  :defer t
+  :after
+  (clj-refactor)
+  (cider)
+  :hook
+  (clojure-mode . cider-mode)
+  (clojure-mode . yas-minor-mode)
+  (clojure-mode . clj-refactor-mode))
