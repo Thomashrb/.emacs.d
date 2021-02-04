62fa675ce67195ddcd32bef6436a9de5954141da
Thomas <thomas@beat.no>
2021-02-04 09:57:46 +0100

    git-init.el: add keychain-environment fix loading ssh keys via agent with zsh

--- lisp/git-init.el
+++ lisp/git-init.el
@@ -8,4 +8,19 @@
   (setq magit-auto-revert-mode nil)
   (global-set-key (kbd "C-c m s") 'magit-status)
   (global-set-key (kbd "C-c m l") 'magit-log)
-  (global-git-gutter-mode t))
+  (global-git-gutter-mode t)
+  :config
+  (with-eval-after-load 'magit
+    (require 'forge)))
+
+;; Work with Git forges, such as Github and Gitlab, from the comfort of Magit and the rest of Emacs.
+;; depends on authinfo
+(use-package forge
+  :defer t
+  :after (magit)
+  :config
+  (add-to-list 'forge-alist '("gitlab.dev.beat.no" "gitlab.dev.beat.no/api/v4" "gitlab.dev.beat.no" forge-gitlab-repository)))
+
+(use-package keychain-environment
+  :init
+  (keychain-refresh-environment))
