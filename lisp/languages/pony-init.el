ce8108fd11702c28cdbc82677ca2d35de3c11f06
Thomas <thomas@beat.no>
2021-11-26 22:18:58 +0100

    pony-init.el: add keybinds

--- lisp/languages/pony-init.el
+++ lisp/languages/pony-init.el
@@ -1,7 +1,20 @@
-(use-package flycheck-pony)
+(use-package flycheck-pony
+  :config
+  (setq create-lockfiles nil))
 
 (use-package ponylang-mode
-  :config
-  (setq create-lockfiles nil)
   :after
-  (flycheck-pony))
+  (flycheck-pony)
+  :config
+  (setq ponylang-use-ctags t)
+  (setq ponylang-format-on-save t)
+  (add-hook
+   'ponylang-mode-hook
+   (lambda ()
+     (set-variable 'indent-tabs-mode nil)
+     (set-variable 'tab-width 2)))
+  :bind
+  (:map ponylang-mode-map
+        ("C-c C-v" . ponylang-project-build)
+        ("C-c C-c" . ponylang-project-run)
+        ("<C-tab>" . company-ctags)))
