cc0bdcd28e767737ef9da60ae08cd2fcb86ce709
Thomas <thomas@beat.no>
2021-11-28 15:31:15 +0100

    org.el: add `todo-keywords`

--- lisp/org.el
+++ lisp/org.el
@@ -11,6 +11,11 @@
  '((shell . t)))
 
 
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "|" "DONE(d)")
+        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
+        (sequence "|" "CANCELED(c)")))
+
 ;; (make-directory "~/.org-jira")
 (use-package org-jira
   :config
