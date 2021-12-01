50c9de46327bb2c06c0eaeeb1047d9b9db70cfa2
Thomas <thomas@beat.no>
2021-12-01 23:51:00 +0100

    helm.el: add etag keybind

--- lisp/helm.el
+++ lisp/helm.el
@@ -7,7 +7,8 @@
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x b") 'helm-mini)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  (global-set-key (kbd "M-y") 'helm-show-kill-ring))
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+  (global-set-key (kbd "C-c p j") 'helm-etags-select))
 
 (use-package helm-swoop
   :config
