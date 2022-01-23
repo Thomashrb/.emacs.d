d70f929e5ae7307f69c4d83dcc2f8b62f42ea2f5
Thomas <thomas@beat.no>
2022-01-23 23:47:51 +0100

    config-init.el: add flycheck-yamllint

--- lisp/languages/config-init.el
+++ lisp/languages/config-init.el
@@ -3,19 +3,28 @@
 ;;; Handle configuration languages
 ;;; Code:
 (use-package yaml-mode
-  :mode (("\\.yml\\'" . yaml-mode)
-	 ("\\.yaml\\'" . yaml-mode)))
+  :mode
+  (("\\.yml\\'" . yaml-mode)
+  (("\\.yaml\\'" . yaml-mode))))
+
+(use-package flycheck-yamllint
+  :init
+  (progn
+    (eval-after-load 'flycheck
+      '(add-hook 'flycheck-mode-hook 'flycheck-yamllint-setup))))
 
 (use-package ansible)
 
 (use-package toml-mode
   :mode (("\\.toml\\'" . yaml-mode)))
 
-
-(use-package json-mode
-  :mode (("\\.json\\'" . json-mode)))
-
 (use-package json-reformat)
 
-(use-package json-snatcher)
+(use-package json-mode
+  :after json-reformat
+  :mode (("\\.json\\'" . json-mode))
+  :bind
+  (:map json-mode-map
+      ("C-c C-f" . json-reformat-region)))
+
 ;;; config-init.el ends here
