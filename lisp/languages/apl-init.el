f9585effe59d9f9657be3907fee78cf00ba9c8fc
Thomas <thomas@beat.no>
2022-01-29 19:08:10 +0100

    apl-init.el: add apl "repl" key

--- lisp/languages/apl-init.el
+++ lisp/languages/apl-init.el
@@ -13,5 +13,8 @@
   (setq text-scale-mode-step 10)
   (setq gnu-apl-show-tips-on-start nil)
   (setq buffer-face-mode-face 'gnu-apl-default)
+  :bind
+  (:map gnu-apl-mode-map
+      ("C-x j j" . gnu-apl))
   :hook buffer-face-mode
   :mode "\\.apl\\'")
