2f92f9e77f424cd9061d596659b903aac417b734
Thomas <thomas@beat.no>
2022-02-04 10:32:07 +0100

    org.el: add org-roam

--- lisp/org.el
+++ lisp/org.el
@@ -6,6 +6,8 @@
 ;; #+MACRO: clientname Jane Doe
 ;; On export {{{clientname}}} will expand to Jane Doe.
 ;;; Code:
+(use-package org)
+
 (use-package ob-restclient) ; org-babel restclient
 
 (org-babel-do-load-languages
@@ -17,8 +19,8 @@
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "DONE(d)")))
 
-
 ;;(make-directory "~/.org-jira")
+(load "~/.emacs.d/tokens/jira.el" )
 (use-package org-jira
   :config
   (setq jiralib-url "https://beattech.atlassian.net")
@@ -37,4 +39,19 @@
                  and sprint in openSprints()"
                 :filename "CONTENT_me"))))
 
+(use-package org-roam
+  :custom
+  (org-roam-directory (file-truename "~/mydocuments/org-files/"))
+  :bind (("C-c o r l" . org-roam-buffer-toggle)
+         ("C-c o r f" . org-roam-node-find)
+         ("C-c o r g" . org-roam-graph)
+         ("C-c o r i" . org-roam-node-insert)
+         ("C-c o r c" . org-roam-capture)
+         ;; Dailies
+         ("C-c o r j" . org-roam-dailies-capture-today))
+  :config
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
 ;;; org.el ends here
