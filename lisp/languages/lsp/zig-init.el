728cc705834218004554ef3f9272c40ffac33c9b
Thomas <thomas@beat.no>
2022-02-13 14:17:05 +0100

    zig-init: add zig build run function

--- lisp/languages/lsp/zig-init.el
+++ lisp/languages/lsp/zig-init.el
@@ -1,6 +1,15 @@
+(defun zig-build-run ()
+  "Execute zig build run."
+  (interactive)
+  (zig--run-cmd "build run" nil))
+
 (use-package zig-mode
+  :config
+  (setq zig-format-on-save nil)
+  (setq zig-ansi-color-for-format-errors t)
   :bind
   (:map zig-mode-map
+        ("M-C-x" . zig-build-run)
         ("C-c C-k" . zig-run)
         ("C-c C-C" . zig-compile)
         ("C-c C-t" . zig-test-buffer)))
