7bcb43b75ae881b32c323c1b8f00bf7d7a7d9dbb
Thomas <thomas@beat.no>
2022-03-01 23:08:06 +0100

    adventofcode.el: load token from file

--- lisp/challenges/adventofcode.el
+++ lisp/challenges/adventofcode.el
@@ -1,2 +1,5 @@
-(use-package elaocd
-  :straight (elaocd :type git :host github :repo "thomashrb/el-aocd"))
+(load "~/.emacs.d/request/aoc.el")
+
+(use-package el-aocd
+  :straight (el-aocd :type git :host github :repo "thomashrb/el-aocd")
+  :hook elaocd-login)
