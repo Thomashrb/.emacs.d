ee13f1d557564a3dd8274d465275af24af5aed6b
Thomas <thomas@beat.no>
2023-12-04 21:53:28 +0100

    add spaceline

--- lisp/appearance/theme.el
+++ lisp/appearance/theme.el
@@ -4,9 +4,17 @@
 
 (use-package spacemacs-theme
   :ensure t
-  :theme
+  :init
   (load-theme 'spacemacs-dark t)
   (setq spacemacs-theme-org-agenda-height nil)
   (setq spacemacs-theme-org-height nil))
+
+(use-package spaceline
+  :demand t
+  :init
+  (setq powerline-default-separator 'arrow-fade)
+  :config
+  (require 'spaceline-config)
+  (spaceline-spacemacs-theme))
 
 ;;; theme.el ends here
