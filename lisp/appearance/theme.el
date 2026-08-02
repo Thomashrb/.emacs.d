e0b0dfccafbd37af9f0f106a6fabf1816a07f990
Thomas <thomas@beat.no>
2026-08-02 18:07:46 +0000

    theme: use moe

--- lisp/appearance/theme.el
+++ lisp/appearance/theme.el
@@ -2,8 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package leuven-theme
+(use-package moe-theme
   :config
-  (load-theme 'leuven-dark t))
+  (load-theme 'moe-dark t)
+  (setq moe-theme-highlight-buffer-id t)
+  (setq moe-theme-resize-title-markdown '(1.5 1.4 1.3 1.2 1.0 1.0))
+  (setq moe-theme-resize-title-org '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+  (setq moe-theme-resize-title-rst '(1.5 1.4 1.3 1.2 1.1 1.0))
+  (setq moe-theme-modeline-color 'cyan))
 
 ;;; theme.el ends here
