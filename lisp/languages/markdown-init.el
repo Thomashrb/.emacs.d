bc99c9129cd094c64ac369ae46124499b9fd121a
thomas <thomashrb@protonmail.com>
2026-08-04 15:49:31 +0000

    markdown-init: fix M-n / M-p being overridden in markdown mode

--- lisp/languages/markdown-init.el
+++ lisp/languages/markdown-init.el
@@ -6,6 +6,13 @@
   :mode ("README\\.md\\'" . gfm-mode)
   :init (setq markdown-command "multimarkdown")
   :bind (:map markdown-mode-map
-         ("C-c C-e" . markdown-do)))
+         ("C-c C-e" . markdown-do)
+         ;; markdown-mode binds M-n/M-p locally, overriding the global
+         ("M-n" . forward-paragraph)
+         ("M-p" . backward-paragraph))
+  :config
+  ;; GFM has its own keymap, so restore the bindings there as well.
+  (define-key gfm-mode-map (kbd "M-n") #'forward-paragraph)
+  (define-key gfm-mode-map (kbd "M-p") #'backward-paragraph))
 
 ;;; markdown-init.el ends here
