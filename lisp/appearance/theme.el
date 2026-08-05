c7c4988e49ca3158e803d7deca495ff3ff3320be
thomas <thomashrb@protonmail.com>
2026-08-05 07:11:26 +0000

    theme: use spacemacs

--- lisp/appearance/theme.el
+++ lisp/appearance/theme.el
@@ -2,13 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package moe-theme
-  :config
-  (load-theme 'moe-dark t)
-  (setq moe-theme-highlight-buffer-id t)
-  (setq moe-theme-resize-title-markdown '(1.5 1.4 1.3 1.2 1.0 1.0))
-  (setq moe-theme-resize-title-org '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
-  (setq moe-theme-resize-title-rst '(1.5 1.4 1.3 1.2 1.1 1.0))
-  (setq moe-theme-modeline-color 'cyan))
+(use-package spacemacs-theme
+  :init
+  (load-theme 'spacemacs-dark t)
+  (setq spacemacs-theme-org-agenda-height nil)
+  (setq spacemacs-theme-org-height nil))
 
 ;;; theme.el ends here
