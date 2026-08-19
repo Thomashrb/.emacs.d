4c90154a8d0136197041c37dff19ce32445bbc18
thomas <thomashrb@protonmail.com>
2026-08-19 21:32:50 +0000

    theme: modus

--- lisp/appearance/theme.el
+++ lisp/appearance/theme.el
@@ -2,10 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package spacemacs-theme
-  :init
-  (load-theme 'spacemacs-dark t)
-  (setq spacemacs-theme-org-agenda-height nil)
-  (setq spacemacs-theme-org-height nil))
+(use-package modus-themes
+  :config
+  (setq modus-themes-mixed-fonts t
+        modus-themes-variable-pitch-ui t
+        modus-themes-italic-constructs t
+        modus-themes-bold-constructs t
+        modus-themes-completions '((t . (bold)))
+        modus-themes-prompts '(bold)
+        modus-themes-headings
+        '((agenda-structure . (variable-pitch light 2.2))
+          (agenda-date . (variable-pitch regular 1.3))
+          (t . (regular 1.15))))
+
+  (setq modus-themes-common-palette-overrides nil)
+  (modus-themes-load-theme 'modus-vivendi))
 
 ;;; theme.el ends here
